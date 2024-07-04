7bf0c3d24414c4ecd1afabab00ea59deb7d22d80
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-04 21:14:13 +0530

    feat: updated init db

--- database/init.sql
+++ database/init.sql
@@ -1,38 +1,51 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE Complains
+CREATE TABLE user
 (
-    comp_id       UUID NOT NULL DEFAULT uuid_generate_v4(),
-    comp_nos      integer NOT NULL,
-    comp_loc      text NOT NULL,
-    comp_des      text NOT NULL,
-    comp_stat     text NOT NULL,
-    comp_date     timestamp(6) with time zone NOT NULL,
-    fin_datetime  timestamp(6) with time zone NOT NULL,
-    fin_info      text NOT NULL,
-    CONSTRAINT PK_Complains PRIMARY KEY (comp_id, comp_nos)
+    user_id  uuid DEFAULT uuid_generate_v4() NOT NULL,
+    username text NOT NULL,
+    pswd     text NOT NULL,
+    role     text NOT NULL,
+    CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE public.inventory
+CREATE TABLE inventory
 (
-    item_id     UUID NOT NULL DEFAULT uuid_generate_v4(),
-    item_name   text NOT NULL,
-    item_qty    numeric NOT NULL,
-    item_price  numeric NOT NULL,
-    CONSTRAINT PK_Inventory PRIMARY KEY (item_id)
+    item_id    uuid DEFAULT uuid_generate_v4() NOT NULL,
+    item_name  text NOT NULL,
+    item_qty   numeric NOT NULL,
+    item_price numeric NOT NULL,
+    item_desc  text NOT NULL,
+    CONSTRAINT PK_inventory PRIMARY KEY (item_id)
+);
+
+CREATE TABLE complains
+(
+    comp_id      uuid DEFAULT uuid_generate_v4() NOT NULL,
+    comp_nos     text NOT NULL,
+    comp_loc     text NOT NULL,
+    comp_des     text NOT NULL,
+    comp_stat    text NOT NULL,
+    comp_date    timestamp(6) with time zone NOT NULL,
+    fin_datetime timestamp(6) with time zone NOT NULL,
+    fin_text     text NOT NULL,
+    CONSTRAINT PK_complains PRIMARY KEY (comp_id, comp_nos)
 );
 
 CREATE TABLE inven_used
 (
-    id          UUID NOT NULL DEFAULT uuid_generate_v4(),
-    item_id     UUID NOT NULL,
-    comp_id     UUID NOT NULL,
-    comp_nos    integer NOT NULL,
-    item_used   integer NOT NULL,
-    CONSTRAINT PK_Inven_Used PRIMARY KEY (id, item_id, comp_id, comp_nos),
-    CONSTRAINT FK_Inven_Used_Item FOREIGN KEY (item_id) REFERENCES public.inventory(item_id),
-    CONSTRAINT FK_Inven_Used_Complain FOREIGN KEY (comp_id, comp_nos) REFERENCES Complains(comp_id, comp_nos)
+    id        uuid DEFAULT uuid_generate_v4() NOT NULL,
+    user_id   uuid NOT NULL,
+    comp_id_1 uuid NOT NULL,
+    comp_nos  text NOT NULL,
+    item_id_1 uuid NOT NULL,
+    item_used numeric NOT NULL,
+    CONSTRAINT PK_inven_used PRIMARY KEY (id, user_id, comp_id_1, comp_nos, item_id_1),
+    CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES user (user_id),
+    CONSTRAINT FK_complains FOREIGN KEY (comp_id_1, comp_nos) REFERENCES complains (comp_id, comp_nos),
+    CONSTRAINT FK_inventory FOREIGN KEY (item_id_1) REFERENCES inventory (item_id)
 );
 
-CREATE INDEX IDX_Inven_Used_Item ON inven_used(item_id);
-CREATE INDEX IDX_Inven_Used_Complain ON inven_used(comp_id, comp_nos);
+CREATE INDEX IDX_item_id_1 ON inven_used (item_id_1);
+CREATE INDEX IDX_comp_id_nos ON inven_used (comp_id_1, comp_nos);
+CREATE INDEX IDX_user_id ON inven_used (user_id);
