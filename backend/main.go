172d070abfa87e4c42077bca295040a3028116af
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-04 22:32:18 +0530

    feat: added env vars

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err.Error())
+	}
 	log.Println("Listening on port: 8000")
 	if err := http.ListenAndServe("0.0.0.0:8000", http.DefaultServeMux); err != nil {
 		log.Panicln(err.Error())
