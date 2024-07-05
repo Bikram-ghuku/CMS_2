7b3a98026345d49ce792562074d22c8a72efa341
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-05 11:40:50 +0530

    feat: updated inventory model

--- backend/models/Inventory.go
+++ backend/models/Inventory.go
@@ -3,9 +3,9 @@ package models
 import "github.com/google/uuid"
 
 type Inventory struct {
-	ItemID    uuid.UUID `db:"item_id"`
-	ItemName  string    `db:"item_name"`
-	ItemQty   float64   `db:"item_qty"`
-	ItemPrice float64   `db:"item_price"`
-	ItemDesc  string    `db:"item_desc"`
+	ItemID    uuid.UUID `db:"item_id" json:"item_id"`
+	ItemName  string    `db:"item_name" json:"item_name"`
+	ItemQty   float64   `db:"item_qty" json:"item_qty"`
+	ItemPrice float64   `db:"item_price" json:"item_price"`
+	ItemDesc  string    `db:"item_desc" json:"item_desc"`
 }
