af1558255553163fd7d186e17f4e3b8398ab114a
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-05 13:45:41 +0530

    feat: adjust models according to sql

--- backend/models/Complaints.go
+++ backend/models/Complaints.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,16 +11,16 @@ type CompStatus string
 
 const (
 	CompOpen  CompStatus = "open"
-	CompClose CompStatus = "close"
+	CompClose CompStatus = "closed"
 )
 
 type Complaint struct {
-	CompID      uuid.UUID  `db:"comp_id"`
-	CompNos     string     `db:"comp_nos"`
-	CompLoc     string     `db:"comp_loc"`
-	CompDes     string     `db:"comp_des"`
-	CompStat    CompStatus `db:"comp_stat"`
-	CompDate    time.Time  `db:"comp_date"`
-	FinDatetime time.Time  `db:"fin_datetime"`
-	FinText     string     `db:"fin_text"`
+	CompID      uuid.UUID      `db:"comp_id" json:"comp_id"`
+	CompNos     string         `db:"comp_nos" json:"comp_nos"`
+	CompLoc     string         `db:"comp_loc" json:"comp_loc"`
+	CompDes     string         `db:"comp_des" json:"comp_des"`
+	CompStat    CompStatus     `db:"comp_stat" json:"comp_stat"`
+	CompDate    time.Time      `db:"comp_date" json:"comp_date"`
+	FinDatetime sql.NullTime   `db:"fin_datetime" json:"fin_datetime"`
+	FinText     sql.NullString `db:"fin_text" json:"fin_text"`
 }
