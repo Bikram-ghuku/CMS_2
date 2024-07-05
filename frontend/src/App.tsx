19a82fc043234671e6040ba5b8efe3ae9459df2d
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-06 00:34:48 +0530

    feat: added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Login from './pages/Login'
+import Home from './pages/Home'
+import Complaints from './pages/Complaints'
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path='/login' Component={Login}/>
+          <Route path='/dashboard' Component={Home} />
+          <Route path='/complaints' Component={Complaints} />
         </Routes>
       </Router>
     </>
