d40f1c729a39108d7449d3278c7ed69960be5fbe
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-05 21:18:13 +0530

    feat: added login screen

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,48 @@
+  // Login.js
+
+  import { useState } from 'react';
+  import './Login.scss';
+  import logo from '../assets/IIT_Kharagpur_Logo.png'
+
+    const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+    return (
+      <div className="login-container">
+        <div className="login-box">
+          <img src={logo} alt="IIT Kharagpur Logo" className="logo" />
+          <h2>CIMS Login</h2>
+          <div className='form'>
+            <div className="form-group">
+              <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Username'
+              required
+              />
+            </div>
+            <div className="form-group">
+              <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+              required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Login;
