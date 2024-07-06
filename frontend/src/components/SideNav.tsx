b4087f4b472f7c6ae85e5c1ff14bc4254f487f17
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-06 10:38:12 +0530

    feat: added sidenav component

---
+++ frontend/src/components/SideNav.tsx
@@ -0,0 +1,44 @@
+import { Home, User, LogOut, Megaphone, Package, PackageOpen } from 'lucide-react';
+import '../styles/SideNav.scss';
+import { useState } from 'react';
+
+const SideNav = () => {
+  	const [isOpen, _] = useState(true);
+	return (
+    	<div className={`sidenav ${isOpen ? 'open' : 'closed'}`}>
+      		<div className="title">
+        		CIMS
+      		</div>
+      		<nav>
+        		<ul>
+          			<li>
+						<Home />
+						<span>Home</span>
+					</li>
+        			<li>
+						<User />
+						<span>Users</span>
+          			</li>
+					<li>
+						<Megaphone />
+						<span>Complaints</span>
+					</li>
+					<li>
+						<Package />
+						<span>Inventory</span>
+					</li>
+					<li>
+						<PackageOpen />
+						<span>Inventory Used</span>
+					</li>
+          			<li>
+            			<LogOut />
+            			<span>Logout</span>
+          			</li>
+        		</ul>
+      		</nav>
+    	</div>
+  	);
+};
+
+export default SideNav;
