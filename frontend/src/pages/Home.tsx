eb5a2d00c5bde5559ff942cdf90324fb59542dc2
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-06 12:13:29 +0530

    feat: added home dashboard

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { jwtDecode } from "jwt-decode";
 import { getCookie} from 'typescript-cookie'
 import '../styles/Home.scss'
+import SideNav from "../components/SideNav";
+import { Boxes, Megaphone, User, Users } from "lucide-react";
 
 type claims = {
     uname:string,
@@ -14,22 +16,52 @@ function Home() {
         document.location = "./complaints"
     }
     return (
-        <div>
-            <div className="home-top"></div>
-            <div className="home-opts">
-                <div className="opts-items-wrap">
-                    <div className="opts-item">
-                        <div className="opts-item-title">Complaints</div>
+        <div className="home-main">
+            <div className="home-side-nav">
+                <SideNav/>
+            </div>
+            <div className="home">
+                <div className="home-top">
+                    <User />
+                    {claimsJson.uname}
+                </div>
+                <div className="home-opts">
+                    <div className="opts-title">
+                        Dashboard
                     </div>
-                    <div className="opts-item">
-                        <div className="opts-item-title">Inventory</div>
-                    </div>
-                    <div className="opts-item">
-                        <div className="opts-item-title">Users</div>
+                    <div className="opts-items-wrap">
+                        <div className="opts-item item-green">
+                            <div className="opt-item-left">
+                                <div className="opts-item-title">Complaints</div>
+                                <div className="opts-item-nos">2</div>
+                            </div>
+                            <div className="opts-item-icon">
+                                <Megaphone size={100} strokeWidth={1}/>
+                            </div>
+                        </div>
+                        <div className="opts-item item-blue">
+                            <div className="opt-item-left">
+                                <div className="opts-item-title">Inventory</div>
+                                <div className="opts-item-nos">2</div>
+                            </div>
+                            <div className="opts-item-icon">
+                                <Boxes size={100} strokeWidth={1} />
+                            </div>
+                        </div>
+                        <div className="opts-item item-yellow">
+                            <div className="opt-item-left">
+                                <div className="opts-item-title">Users</div>
+                                <div className="opts-item-nos">2</div>
+                            </div>
+                            <div className="opts-item-icon">
+                                <Users size={100} strokeWidth={1}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div className="home-inven-use"></div>
+                <div className="home-footer"></div>
             </div>
-            <div className="home-footer"></div>
         </div>
     )
 }
