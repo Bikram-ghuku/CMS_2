3297b1bb2eeb5fa360c8c58cdfef98237646dd9e
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-06 13:03:34 +0530

    feat: added numbers fetch from backend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,19 +2,51 @@ import { jwtDecode } from "jwt-decode";
 import { getCookie} from 'typescript-cookie'
 import '../styles/Home.scss'
 import SideNav from "../components/SideNav";
-import { Boxes, Megaphone, User, Users } from "lucide-react";
+import { Boxes, Loader2, Megaphone, User, Users } from "lucide-react";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../constants";
+import { ToastContainer, toast } from 'react-toastify';
 
 type claims = {
     uname:string,
     role:string
 }
+
+type nums = {
+    inven_nos:number,
+    comp_nos:number,
+    user_nos:number
+}
 function Home() {
     const sessionCookie = getCookie("session-token")
     const claimsJson:claims = jwtDecode(sessionCookie!)
-    console.log(claimsJson.uname)
+    const [load, setIsLoad] = useState(true)
+    const [numData, setNumData] = useState<nums>({
+        inven_nos:0,
+        comp_nos:0,
+        user_nos:0
+    })
     if(claimsJson.role == "worker"){
         document.location = "./complaints"
     }
+
+    useEffect(()=>{
+        fetch(BACKEND_URL+"/stat/num", {
+            method:"GET",
+            credentials: "include"
+        }).then((data) => {
+            setIsLoad(false)
+            if(data.ok){
+                data.json().then((data) => {
+                    setNumData(data)
+                })
+            }else{
+                toast.error("Loading Error", {
+                    position: "bottom-center"
+              })
+            }
+        })
+    }, [])
     return (
         <div className="home-main">
             <div className="home-side-nav">
@@ -33,35 +65,39 @@ function Home() {
                         <div className="opts-item item-green">
                             <div className="opt-item-left">
                                 <div className="opts-item-title">Complaints</div>
-                                <div className="opts-item-nos">2</div>
+                                <div className="opts-item-nos">{numData.comp_nos}</div>
                             </div>
                             <div className="opts-item-icon">
-                                <Megaphone size={100} strokeWidth={1}/>
+                            {load ? <Loader2 className='animate-spin' size={100} /> : <Megaphone size={100} strokeWidth={1}/>}
                             </div>
                         </div>
                         <div className="opts-item item-blue">
                             <div className="opt-item-left">
                                 <div className="opts-item-title">Inventory</div>
-                                <div className="opts-item-nos">2</div>
+                                <div className="opts-item-nos">{numData.inven_nos}</div>
                             </div>
                             <div className="opts-item-icon">
-                                <Boxes size={100} strokeWidth={1} />
+                            {load ? <Loader2 className='animate-spin' size={100} /> : <Boxes size={100} strokeWidth={1} />}
                             </div>
                         </div>
                         <div className="opts-item item-yellow">
                             <div className="opt-item-left">
                                 <div className="opts-item-title">Users</div>
-                                <div className="opts-item-nos">2</div>
+                                <div className="opts-item-nos">{numData.user_nos}</div>
                             </div>
                             <div className="opts-item-icon">
-                                <Users size={100} strokeWidth={1}/>
+                                {load ? <Loader2 className='animate-spin' size={100} /> : <Users size={100} strokeWidth={1}/>}
                             </div>
                         </div>
                     </div>
                 </div>
+                <hr className="home-hr"/>
                 <div className="home-inven-use"></div>
-                <div className="home-footer"></div>
+                <div className="home-footer">
+                        Copyright © Sanitary Section, IIT Kharagpur.
+                </div>
             </div>
+            <ToastContainer/>
         </div>
     )
 }
