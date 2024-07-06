d2043c2db30aca3ae241462b1583fd5ea61e683e
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-07 00:17:40 +0530

    feat: added footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>Copyright © Sanitary Section, IIT Kharagpur.</p>
+    </footer>
+  );
+};
+
+export default Footer;
