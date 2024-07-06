392034fd4f98c7b2b0adf26d4e9bf83b78cd9ab6
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-06 21:14:22 +0530

    feat: update indention

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,21 +7,21 @@ import Complaint from "./pages/Complaint"
 import Inventory from "./pages/Inventory"
 import Invenused from "./pages/Invenused"
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/login' Component={Login}/>
-          <Route path='/dashboard' Component={Home} />
-          <Route path='/complaints' Component={Complaints} />
-          <Route path='/users' Component={Users} />
-          <Route path='/complaint' Component={Complaint} />
-          <Route path='/inventory' Component={Inventory} />
-          <Route path='/invenused' Component={Invenused} />
-        </Routes>
-      </Router>
-    </>
-  )
+	return (
+    	<>
+    		<Router>
+        		<Routes>
+          			<Route path='/login' Component={Login}/>
+          			<Route path='/dashboard' Component={Home} />
+          			<Route path='/complaints' Component={Complaints} />
+          			<Route path='/users' Component={Users} />
+          			<Route path='/complaint' Component={Complaint} />
+          			<Route path='/inventory' Component={Inventory} />
+          			<Route path='/invenused' Component={Invenused} />
+        		</Routes>
+      		</Router>
+    	</>
+  	)
 }
 
 export default App
