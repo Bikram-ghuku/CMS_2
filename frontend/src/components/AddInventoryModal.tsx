d2f4e119eb334939be16c1bbc03a887c53a6da89
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-07 11:31:19 +0530

    feat: made add Inventory modal as a component

---
+++ frontend/src/components/AddInventoryModal.tsx
@@ -0,0 +1,107 @@
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+import { toast } from 'react-toastify';
+import { BACKEND_URL } from '../constants'
+
+
+interface AddInventoryModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+const AddInventoryModal: React.FC<AddInventoryModalProps> = ({
+    isOpen,
+    onRequestClose
+}) => {
+    const [newItemName, setNewItemName] = useState<string>('');
+    const [newPrice, setNewPrice] = useState<number>(0);
+    const [newQty, setNewQTy] = useState<number>(0);
+    const [newDesc, setNewDesc] = useState<string>('');
+
+
+    const handleRegister = () => {
+        const dateTime = new Date()
+        console.log(dateTime.toUTCString())
+        fetch(BACKEND_URL+"/inven/addItem", {
+            method:"POST",
+            credentials:"include",
+            body:JSON.stringify({item_name: newItemName, item_qty: newQty, item_price: newPrice, item_desc: newDesc})
+        }).then((data) => {
+            setNewItemName('')
+            setNewPrice(0)
+            setNewQTy(0);
+            setNewDesc('')
+            onRequestClose()
+            if(data.ok){
+                toast.success("New item added successfully", {
+                    position: "bottom-center"
+                })
+            }else{
+                toast.error("Error adding new item", {
+                    position: "bottom-center"
+                })
+            }
+        })
+    };
+
+    const handleItemNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewItemName(event.target.value);
+    };
+
+    const handleQtyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewQTy(Number(event.target.value));
+    };
+
+    const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewPrice(Number(event.target.value));
+    };
+
+    const handleDescChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewDesc(event.target.value);
+    };
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="New Inventory Modal"
+            className="modal"
+            overlayClassName="overlay"
+        >
+            <h2>New Inventory</h2>
+            <form>
+                <label>Item Name:</label>
+                <input
+                    type="text"
+                    value={newItemName}
+                    onChange={handleItemNameChange}
+                    placeholder='Enter Item name'
+                />
+                <div className="input-sm-line">
+                    <div className="input-grp">
+                        <label>Item Quantity:</label>
+                        <input
+                            type="number"
+                            value={newQty}
+                            onChange={handleQtyChange}
+                            placeholder='Enter Item Quantity'
+                        />
+                    </div>
+                    <div className="input-grp">
+                        <label>Item Price:</label>
+                        <input
+                            type="number"
+                            value={newPrice}
+                            onChange={handlePriceChange}
+                            placeholder='Enter Item Price'
+                        />
+                    </div>
+                </div>
+                <label>Item Description:</label>
+                <textarea value={newDesc} onChange={handleDescChange} placeholder='Enter Item Description'></textarea>
+                <button type="button" onClick={handleRegister}>Add Item</button>
+            </form>
+        </Modal>
+    );
+}
+
+export default AddInventoryModal;
--- frontend/src/pages/Inventory.tsx
+++ frontend/src/pages/Inventory.tsx
@@ -6,8 +6,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 import "../styles/Inventory.scss"
 import { Plus } from 'lucide-react';
-import Modal from 'react-modal'
 import Footer from '../components/Footer';
+import AddInventoryModal from '../components/AddInventoryModal';
 
 type item = {
     item_name:string,
@@ -23,10 +23,7 @@ function Inventory() {
     const [filteredComps, setFilteredComps] = useState<item[]>([]);
     const [searchInput, setSearchInput] = useState<string>('');
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-    const [newItemName, setNewItemName] = useState<string>('');
-    const [newPrice, setNewPrice] = useState<number>(0);
-    const [newQty, setNewQTy] = useState<number>(0);
-    const [newDesc, setNewDesc] = useState<string>('');
+
 
     useEffect(() => {
         fetch(BACKEND_URL+'/inven/all', {
@@ -66,48 +63,6 @@ function Inventory() {
 
     const closeModal = () => {
         setIsModalOpen(false);
-    };
-
-    const handleItemNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewItemName(event.target.value);
-    };
-
-    const handleQtyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewQTy(Number(event.target.value));
-    };
-
-    const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewPrice(Number(event.target.value));
-    };
-
-    const handleDescChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setNewDesc(event.target.value);
-    };
-
-
-    const handleRegister = () => {
-        const dateTime = new Date()
-        console.log(dateTime.toUTCString())
-        fetch(BACKEND_URL+"/inven/addItem", {
-            method:"POST",
-            credentials:"include",
-            body:JSON.stringify({item_name: newItemName, item_qty: newQty, item_price: newPrice, item_desc: newDesc})
-        }).then((data) => {
-            setIsModalOpen(false)
-            setNewItemName('')
-            setNewPrice(0)
-            setNewQTy(0);
-            setNewDesc('')
-            if(data.ok){
-                toast.success("New item added successfully", {
-                    position: "bottom-center"
-                })
-            }else{
-                toast.error("Error adding new item", {
-                    position: "bottom-center"
-                })
-            }
-        })
     };
 
     return (
@@ -161,47 +116,7 @@ function Inventory() {
                     </table>
                 </div>
             </div>
-            <Modal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                contentLabel="New User Modal"
-                className="modal"
-                overlayClassName="overlay"
-            >
-                <h2>New Inventory</h2>
-                <form>
-                    <label>Item Name:</label>
-                    <input
-                        type="text"
-                        value={newItemName}
-                        onChange={handleItemNameChange}
-                        placeholder='Enter Item name'
-                    />
-                    <div className="input-sm-line">
-                        <div className="input-grp">
-                            <label>Item Quantity:</label>
-                            <input
-                                type="number"
-                                value={newQty}
-                                onChange={handleQtyChange}
-                                placeholder='Enter Item Quantity'
-                            />
-                        </div>
-                        <div className="input-grp">
-                            <label>Item Price:</label>
-                            <input
-                                type="number"
-                                value={newPrice}
-                                onChange={handlePriceChange}
-                                placeholder='Enter Item Price'
-                            />
-                        </div>
-                    </div>
-                    <label>Item Description:</label>
-                    <textarea value={newDesc} onChange={handleDescChange} placeholder='Enter Item Description'></textarea>
-                    <button type="button" onClick={handleRegister}>Add Item</button>
-                </form>
-            </Modal>
+            <AddInventoryModal isOpen={isModalOpen} onRequestClose={closeModal}/>
             <ToastContainer/>
             <Footer />
         </div>
