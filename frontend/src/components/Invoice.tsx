58312807be0a6babbf80b3b8a36c0f7001953d56
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-11 16:51:26 +0530

    feat: updated Invoice styles

--- frontend/src/components/Invoice.tsx
+++ frontend/src/components/Invoice.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Invoice.scss';
 import logo from '../assets/logo.png';
+import { ToWords } from 'to-words';
+
 
 type InvenUsed = {
     id: string;
@@ -13,6 +15,7 @@ type InvenUsed = {
     item_qty: number;
     item_price: number;
     item_desc: string;
+    item_unit: string;
     comp_id: string;
     comp_nos: string;
     comp_loc: string;
@@ -30,38 +33,60 @@ const Invoice = React.forwardRef<HTMLDivElement, InvoiceProps>(({ selectedItems 
         return selectedItems.reduce((acc, item) => acc + item.item_used * item.item_price, 0);
     };
 
+    const toWords = new ToWords({
+        localeCode: 'en-IN',
+        converterOptions: {
+            currency: true,
+            ignoreDecimal: false,
+            ignoreZeroCurrency: false,
+            doNotAddOnly: false,
+            currencyOptions: {
+                name: 'Rupee',
+                plural: 'Rupees',
+                symbol: '₹',
+                fractionalUnit: {
+                    name: 'Paisa',
+                    plural: 'Paise',
+                    symbol: '',
+                },
+            },
+        },
+    });
+
     return (
         <div ref={ref} className="invoice-main">
             <div className="invoice-header">
                 <img src={logo} alt="Logo" className="invoice-logo" />
-                <h1>Sanitary Section, IIT Kharagpur</h1>
+                <h2>Institute Works <br/> Sanitary Section <br/> IIT Kharagpur</h2>
             </div>
-            <h2>Invoice</h2>
             <table>
                 <thead>
                     <tr>
-                        <th>Item Name</th>
-                        <th>Item Description</th>
-                        <th>Quantity Used</th>
-                        <th>Complaint Location</th>
-                        <th>Price per Item</th>
-                        <th>Total Price</th>
+                        <th>Sl. No</th>
+                        <th>Name of Item</th>
+                        <th>Description of Item</th>
+                        <th>Unit</th>
+                        <th>Quantity</th>
+                        <th>Rate</th>
+                        <th>Amount (Rs.)</th>
                     </tr>
                 </thead>
                 <tbody>
                     {selectedItems.map((item, idx) => (
                         <tr key={idx}>
+                            <td>{idx + 1}</td>
                             <td>{item.item_name}</td>
                             <td>{item.item_desc}</td>
+                            <td>{item.item_unit}</td>
                             <td>{item.item_used}</td>
-                            <td>{item.comp_loc}</td>
                             <td>{`₹${item.item_price.toFixed(2)}`}</td>
                             <td>{`₹${(item.item_used * item.item_price).toFixed(2)}`}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-            <h3>Total: {`₹${calculateTotal().toFixed(2)}`}</h3>
+            <h3>Total Rs.: {`₹${calculateTotal().toFixed(2)}`}</h3>
+            <h5>{toWords.convert(calculateTotal()) + " (Inclusive of all Taxes)"}</h5>
         </div>
     );
 });
