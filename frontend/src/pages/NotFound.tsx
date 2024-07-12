56b45cd61224b4af5eae778a73166d214ef36931
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-12 20:03:57 +0530

    feat: include logo in error page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import '../styles/NotFound.scss';
+import logo from '../assets/IIT_Kharagpur_Logo.png'
+
 
 const NotFound: React.FC = () => {
     return (
         <div className="not-found">
+            <img src={logo} alt="CIMS Logo" className="logo" />
             <h1>404</h1>
             <p>Page Not Found</p>
             <a href="/" className="back-home">Go Back Home</a>
