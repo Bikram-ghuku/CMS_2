1aafd308f832bccb031b6a25b7261beced57ebb8
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-12 22:27:15 +0530

    feat: fix indention

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,16 +13,13 @@ import * as Sentry from "@sentry/react";
 Sentry.init({
 	dsn: "https://e9a51c4ebb5c8ece98f591f7f775c77c@o1272929.ingest.us.sentry.io/4507589866291200",
 	integrations: [
-	  Sentry.browserTracingIntegration(),
-	  Sentry.replayIntegration(),
+		Sentry.browserTracingIntegration(),
+	  	Sentry.replayIntegration(),
 	],
-	// Performance Monitoring
-	tracesSampleRate: 1.0, //  Capture 100% of the transactions
-	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracesSampleRate: 1.0,
 	tracePropagationTargets: ["localhost", /^\//],
-	// Session Replay
-	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0,
   });
 
 function App() {
