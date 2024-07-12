a289601930f2b33a17b17b91ad45e9f315ab40b1
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-12 21:57:07 +0530

    feat: added sentry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,23 @@ import Inventory from "./pages/Inventory"
 import Invenused from "./pages/Invenused"
 import LandingPage from "./pages/Landing"
 import NotFound from "./pages/NotFound"
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+	dsn: "https://5653fec6874858b7db8111b3bb6af040@o1272929.ingest.us.sentry.io/4507589639929856",
+	integrations: [
+	  Sentry.browserTracingIntegration(),
+	  Sentry.replayIntegration(),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracePropagationTargets: ["localhost", /^\//],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 function App() {
 	return (
     	<>
