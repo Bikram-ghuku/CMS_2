4326b4d9f3b4fedd1ed49e49f71418547619d90f
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-24 19:16:23 +0530

    feat: added backup scripts

---
+++ backup_scripts/backup.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+
+apk add --no-cache postgresql-client curl
+
+pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_UNAME} ${DB_NAME} > /backups/dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+
+curl -X POST https://content.dropboxapi.com/2/files/upload \
+  --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
+  --header "Dropbox-API-Arg: {\"path\": \"/db_backup.sql\",\"mode\": \"overwrite\"}" \
+  --header "Content-Type: application/octet-stream" \
+  --data-binary @/backups/db_backup.sql
