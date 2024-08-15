f5ee9895177ffa8852be9f867cf8f2881d53dbaa
Bikram Ghuku <bikramghuku05@gmail.com>
2024-08-15 19:38:24 +0530

    feat: new backup script

--- backup_scripts/backup.sh
+++ backup_scripts/backup.sh
@@ -6,12 +6,17 @@
 : "${DB_NAME:?DB_NAME is not set}"
 : "${DROPBOX_ACCESS_TOKEN:?DROPBOX_ACCESS_TOKEN is not set}"
 
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+BACKUP_FILE="db_backup_${DATE}.sql"
+
 export PGPASSWORD="${DB_PSWD}"
 
-pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_UNAME} ${DB_NAME} > /backups/db_backup.sql
+echo "Creating backup of $DB_NAME..."
+pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_UNAME} ${DB_NAME} > /backups/${BACKUP_FILE}
 
+echo "Uploading backup to Dropbox..."
 curl -X POST https://content.dropboxapi.com/2/files/upload \
   --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
-  --header "Dropbox-API-Arg: {\"path\": \"/db_backup.sql\",\"mode\": \"overwrite\"}" \
+  --header "Dropbox-API-Arg: {\"path\": \"/${BACKUP_FILE}\",\"mode\": \"overwrite\"}" \
   --header "Content-Type: application/octet-stream" \
-  --data-binary @/backups/db_backup.sql
+  --data-binary @/backups/${BACKUP_FILE}
